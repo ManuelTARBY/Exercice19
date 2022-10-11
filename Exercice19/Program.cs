fd4a3d99b085027cbf69d02153cf52b57fa695e3
Manu_31 <Manu_31@PC-MANU2>
2022-10-11 18:58:35 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice19/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Exercice19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entrez un montant = ");
+            float montant = float.Parse(Console.ReadLine());
+            float taux = 0;
+            if (montant > 40)
+            {
+                taux = 0.1f;
+            }
+            else
+            {
+                if (montant >= 20)
+                {
+                    taux = 0.05f;
+                }
+            }
+            montant -= montant * taux;
+            Console.WriteLine("montant = " + montant + " avec une remise de " + (taux * 100) + "%");
+            Console.ReadLine();
+        }
+    }
+}
