95f625c8b0eb8e4ba351e26f97f6c62e7cd43c2f
Manuel Tarby <manuel.tarby@gmail.com>
2022-10-11 20:04:41 +0200

    Insertion d'un bloc gérant les exceptions pour la saisie du montant.

--- Exercice19/Program.cs
+++ Exercice19/Program.cs
@@ -7,22 +7,35 @@ namespace Exercice19
     {
         static void Main(string[] args)
         {
-            Console.Write("Entrez un montant = ");
-            float montant = float.Parse(Console.ReadLine());
+            bool saisieCorrecte = false;
+            float montant = 0f;
+            do
+            {
+                try
+                {
+                    Console.Write("Entrez un montant = ");
+                    montant = float.Parse(Console.ReadLine());
+                    saisieCorrecte = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Entrez une valeur numérique");
+                }
+            } while (saisieCorrecte == false);
             float taux = 0;
             if (montant > 40)
             {
-                taux = 0.1f;
+                taux = 10f;
             }
             else
             {
                 if (montant >= 20)
                 {
-                    taux = 0.05f;
+                    taux = 5f;
                 }
             }
-            montant -= montant * taux;
-            Console.WriteLine("montant = " + montant + " avec une remise de " + (taux * 100) + "%");
+            montant -= montant * taux / 100;
+            Console.WriteLine("Montant = " + montant + " avec une remise de " + taux + "%");
             Console.ReadLine();
         }
     }
